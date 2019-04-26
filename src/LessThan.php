9d029836b15ee5da5f2e9c31ccd8f56f1618067f
Alessandro Tesoro <alessandrotesoro@users.noreply.github.com>
2019-04-26 11:08:19 +0100

    addedd "less than" validation rules

---
+++ src/LessThan.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Posterno validator library.
+ *
+ * @package     posterno-validator
+ * @copyright   Copyright (c) 2019, Pressmodo, LLC
+ * @license     http://opensource.org/licenses/gpl-2.0.php GNU Public License
+ * @since       0.1.0
+ */
+
+/**
+ * @namespace
+ */
+namespace PNO\Validator;
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Make sure the input is less than the specified value.
+ */
+class LessThan extends AbstractValidator {
+
+	/**
+	 * Make sure the input is less than the specified value.
+	 *
+	 * @param mixed $input the value to validate.
+	 * @return mixed
+	 */
+	public function evaluate( $input = null ) {
+		if ( null !== $input ) {
+			$this->input = $input;
+		}
+
+		if ( null === $this->message ) {
+			$this->message = 'The value must be less than ' . $this->value . '.';
+		}
+
+		return ( $this->input < $this->value );
+	}
+
+}
